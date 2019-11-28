253e6de49441c96483b8a507902f86ce414e4cae
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2019-11-28 13:05:52 -0500

    Add skeleton fit and hello functions.
    Add fit function that doesn't actually work yet. Add hello function that
    has a welcome message.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
-    ( someFunc
-    ) where
+    ( FitMode
+    , fit
+    , hello
+    )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+data FitMode
+    = Wrap
+    | Reflect
+    | Range
+    deriving (Show)
+
+fit :: Int -> Int -> Int -> FitMode -> Int
+fit n min max mode = if inRange
+    then n
+    else case mode of
+        Wrap    -> n
+        Reflect -> n
+        Range   -> n
+    where inRange = min <= n && n <= max
+
+hello :: IO ()
+hello = putStrLn "Welcome to Mu, an algorithmic composition toolbox."
