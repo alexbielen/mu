a883bfd82fe1e6dc40d3c60da023f9d457aa17ae
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2019-11-29 02:29:50 -0500

    Refactor fit function.
    Refactor parameter order to support currying. Add functionality and
    tests for Wrap mode.

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,21 +5,33 @@ module Lib
     )
 where
 
-
 data FitMode
     = Wrap
     | Reflect
-    | Range
+    | Limit
     deriving (Show)
 
-fit :: Int -> Int -> Int -> FitMode -> Int
-fit n min max mode = if inRange
+{- 
+fit transforms a value to "fit" within a range according to a mode. 
+
+Wrap uses modular (aka "clock") arithmetic to "wrap" n into the range. 
+
+Limit takes any out-of-range values and replaces with the nearest bound. 
+
+-}
+fit :: FitMode -> Int -> Int -> Int -> Int
+fit mode min max n = if inRange
     then n
     else case mode of
-        Wrap    -> n
+        Wrap ->
+            (if max == nearBound then min else max) + rem (n - nearBound) range
         Reflect -> n
-        Range   -> if n < min then min else max
-    where inRange = min <= n && n <= max
+        Limit   -> nearBound
+  where
+    inRange   = min <= n && n <= max
+    range     = max - min
+    nearBound = if n > max then max else min -- the bound that is closest to n
+
 
 hello :: IO ()
 hello = putStrLn "Welcome to Mu, an algorithmic composition toolbox."
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,22 +5,36 @@ import qualified Lib                            ( FitMode(..)
 
 main :: IO ()
 main = hspec $ describe "fit" $ do
+    let wrapped   = Lib.fit Lib.Wrap
+    let reflected = Lib.fit Lib.Reflect
+    let limited   = Lib.fit Lib.Limit
+    -- Wrap Mode
     it "returns n when n is in between min and max and mode is Wrap"
-        $          Lib.fit 2 1 3 Lib.Wrap
+        $          wrapped 1 3 2
         `shouldBe` 2
 
+    it "returns a wrapped value when n is greater than max and mode is Wrap"
+        $          wrapped 0 6 7
+        `shouldBe` 1
+
+    it "returns a wrapped value when n is less than min and mode is Wrap"
+        $          wrapped 0 6 (-1)
+        `shouldBe` 5
+
+    -- Reflect Mode
     it "returns n when n is in between min and max and mode is Reflect"
-        $          Lib.fit 2 1 3 Lib.Reflect
+        $          reflected 1 3 2
         `shouldBe` 2
 
+    -- Limit mode
     it "returns n when n is in between min and max and mode is Range"
-        $          Lib.fit 2 1 3 Lib.Range
+        $          limited 1 3 2
         `shouldBe` 2
 
     it "returns min when n is less than min and mode is Range"
-        $          Lib.fit (-1) 1 3 Lib.Range
+        $          limited 1 3 (-1)
         `shouldBe` 1
 
     it "returns max when n is greater than max and mode is Range"
-        $          Lib.fit 4 1 3 Lib.Range
+        $          limited 1 3 4
         `shouldBe` 3
