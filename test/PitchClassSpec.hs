cd558faf95057364292af0a4112827768a88669b
Alex Hendrie Bielen <alexhendrebielen@gmail.com>
2019-12-11 01:37:53 -0500

    Simplify PitchClass by deriving Enums for PitchClass and Interval.
    This change allows us to remove the Maybe and the conversion code.

--- test/PitchClassSpec.hs
+++ test/PitchClassSpec.hs
@@ -11,8 +11,8 @@ testInterval :: SpecWith ()
 testInterval = describe "interval"
   $ do
     it "returns Maj3 for C E"
-      $ PC.ascendingInterval PC.C PC.E `shouldBe` Just PC.Maj3
+      $ PC.ascendingInterval PC.C PC.E `shouldBe` PC.Maj3
     it "returns Min2 for B C"
-      $ PC.ascendingInterval PC.B PC.C `shouldBe` Just PC.Min2
+      $ PC.ascendingInterval PC.B PC.C `shouldBe` PC.Min2
     it "returns a unison for D D"
-      $ PC.ascendingInterval PC.D PC.D `shouldBe` Just PC.Unison
+      $ PC.ascendingInterval PC.D PC.D `shouldBe` PC.Unison
